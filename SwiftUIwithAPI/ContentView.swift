f700e50e8e0c0c024f729ded30dae724f8b71e9a
AofPatiphan <patiphan.ma@hotmail.com>
2022-10-20 01:02:31 +0700

    Initial Commit

---
+++ SwiftUIwithAPI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIwithAPI
+//
+//  Created by Patiphan Manawanich on 20/10/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIwithAPI/SwiftUIwithAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIwithAPIApp.swift
+//  SwiftUIwithAPI
+//
+//  Created by Patiphan Manawanich on 20/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIwithAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
